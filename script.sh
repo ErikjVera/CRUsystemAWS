2ad7ac2f79d4805b6dad78a7e01ff50ec720244d
ErikjVera <133960465+ErikjVera@users.noreply.github.com>
2023-11-24 17:53:36 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,37 +1,65 @@
 #!/bin/bash
-# Función para esperar hasta que las instancias estén en estado "running"
-wait_until_running(){
-    local instance_ids=($@)
-    aws ec2 wait instance-running --instance-ids ${instance_ids[@]}
-    }
 
-# Crear una VPC
-vpcId=$(aws ec2 create-vpc --cidr-block 192.168.0.0/22 --query 'Vpc.VpcId' --output text)
+# Variables
+vpc_name="mileserik"
+region="us-east-1"
+cidr_block="192.168.1.0/22"
 
-# Crear subred para el Departamento de Ingeniería
-subnetIdEng=$(aws ec2 create-subnet --vpc-id $vpcId --cidr-block 192.168.0.0/22 --availability-zone us-east-1a --query 'Subnet.SubnetId' --output text)
-# Crear una instancia EC2 para el Departamento de Ingeniería
-aws ec2 run-instances --image-id ami-0230bd60aa48260c6 --subnet-id $subnetIdEng --count 1 --instance-type t2.micro --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=ec2-Ingenieria}]'
-# Esperar hasta que la instancia esté en estado "running"
-wait_until_running $(aws ec2 describe-instances --filters "Name=tag:Name,Values=ec2-Ingenieria" --query 'Reservations[*].Instances[*].InstanceId' --output text)
+# Crear VPC
+vpc_id=$(aws ec2 create-vpc --cidr-block $cidr_block --query 'Vpc.VpcId' --output text --region $region)
+aws ec2 create-tags --resources $vpc_id --tags Key=Name,Value=$vpc_name --region $region
+aws ec2 modify-vpc-attribute --vpc-id $vpc_id --enable-dns-support "{\"Value\":true}" --region $region
+aws ec2 modify-vpc-attribute --vpc-id $vpc_id --enable-dns-hostnames "{\"Value\":true}" --region $region
+echo "VPC creada con ID: $vpc_id"
 
-# Crear subred para el Departamento de Desarrollo
-subnetIdDev=$(aws ec2 create-subnet --vpc-id $vpcId --cidr-block 192.168.1.0/22 --availability-zone us-east-1b --query 'Subnet.SubnetId' --output text)
-# Crear una instancia EC2 para el Departamento de Desarrollo
-aws ec2 run-instances --image-id ami-0230bd60aa48260c6 --subnet-id $subnetIdDev --count 1 --instance-type t2.micro --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=ec2-Desarrollo}]'
-# Esperar hasta que la instancia esté en estado "running"
-wait_until_running $(aws ec2 describe-instances --filters "Name=tag:Name,Values=ec2-Desarrollo" --query 'Reservations[*].Instances[*].InstanceId' --output text)
+# Crear subredes y EC2 para cada departamento
+subnet_index=1
 
-# Crear subred para el Departamento de Mantenimiento
-subnetIdMaint=$(aws ec2 create-subnet --vpc-id $vpcId --cidr-block 192.168.2.0/22 --availability-zone us-east-1c --query 'Subnet.SubnetId' --output text)
-# Crear una instancia EC2 para el Departamento de Mantenimiento
-aws ec2 run-instances --image-id ami-0230bd60aa48260c6 --subnet-id $subnetIdMaint --count 1 --instance-type t2.micro --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=ec2-Mantenimiento}]'
-# Esperar hasta que la instancia esté en estado "running"
-wait_until_running $(aws ec2 describe-instances --filters "Name=tag:Name,Values=ec2-Mantenimiento" --query 'Reservations[*].Instances[*].InstanceId' --output text)
+# Ingeniería
+availability_zone="us-east-1a"
+name="ingenieria"
+subnet_cidr="192.168.$subnet_index.0/24"
+subnet_id=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block $subnet_cidr --availability-zone $availability_zone --query 'Subnet.SubnetId' --output text --region $region)
+instance_name="ec2-$name"
+aws ec2 run-instances --image-id ami-0230bd60aa48260c6 --instance-type t2.micro --subnet-id $subnet_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance_name-$i}]" --region $region
 
-# Crear subred para el Departamento de Soporte
-subnetIdSupp=$(aws ec2 create-subnet --vpc-id $vpcId --cidr-block 192.168.3.0/22 --availability-zone us-east-1d --query 'Subnet.SubnetId' --output text)
-# Crear una instancia EC2 para el Departamento de Soporte
-aws ec2 run-instances --image-id ami-0230bd60aa48260c6 --subnet-id $subnetIdSupp --count 1 --instance-type t2.micro --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=ec2-Soporte}]'
-# Esperar hasta que la instancia esté en estado "running"
-wait_until_running $(aws ec2 describe-instances --filters "Name=tag:Name,Values=ec2-Soporte" --query 'Reservations[*].Instances[*].InstanceId' --output text)
+echo "Subred y EC2 para $name creadas"
+
+subnet_index=$((subnet_index + 1))
+
+# Desarrollo
+
+name="desarrollo"
+
+subnet_cidr="192.168.$subnet_index.0/24"
+subnet_id=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block $subnet_cidr --availability-zone $availability_zone --query 'Subnet.SubnetId' --output text --region $region)
+instance_name="ec2-$name"
+aws ec2 run-instances --image-id ami-0230bd60aa48260c6 --instance-type t2.micro --subnet-id $subnet_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance_name-$i}]" --region $region
+
+echo "Subred y EC2 para $name creadas"
+
+subnet_index=$((subnet_index + 1))
+
+# Mantenimiento
+
+name="mantenimiento"
+subnet_cidr="192.168.$subnet_index.0/24"
+subnet_id=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block $subnet_cidr --availability-zone $availability_zone --query 'Subnet.SubnetId' --output text --region $region)
+instance_name="ec2-$name"
+aws ec2 run-instances --image-id ami-0230bd60aa48260c6 --instance-type t2.micro --subnet-id $subnet_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance_name-$i}]" --region $region
+
+echo "Subred y EC2 para $name creadas"
+
+subnet_index=$((subnet_index + 1))
+
+# Soporte
+
+name="soporte"
+subnet_cidr="192.168.$subnet_index.0/24"
+subnet_id=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block $subnet_cidr --availability-zone $availability_zone --query 'Subnet.SubnetId' --output text --region $region)
+instance_name="ec2-$name"
+aws ec2 run-instances --image-id ami-0230bd60aa48260c6 --instance-type t2.micro --subnet-id $subnet_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance_name-$i}]" --region $region
+
+echo "Subred y EC2 para $name creadas"
+
+echo "Script completado"
